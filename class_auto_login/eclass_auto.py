7b8e9d701bbd49df01735bfb908789fd56356529
xi_jjun <rlawowns97@naver.com>
2021-11-03 00:53:35 +0900

    make data

--- class_auto_login/eclass_auto.py
+++ class_auto_login/eclass_auto.py
@@ -1,14 +1,12 @@
-# 학교 홈페이지 자동으로 들어가서 수업 자동으로 들어가주는 프로그램
-import pyautogui
+import pyautogui as pg
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pyperclip
 import time
 
-path = "/home/xi_jjun/workspace/D-Coder/class_auto_login/chromedriver"
+path = "/home/xi-jjun/ex_workplace/D-Coder/class_auto_login/chromedriver"
 driver = webdriver.Chrome(path)
 driver.set_window_size(1600,1080)
 driver.implicitly_wait(10)
@@ -54,8 +52,8 @@ try:
     driver.switch_to.default_content()
     driver.switch_to.frame(driver.find_element_by_xpath('/html/frameset/frame')) # frame # we have to refer frame - iframe 
 
-    login_ID = '2016'
-    login_PW = ''
+    login_ID = '2016111952'
+    login_PW = '69dvlih32xz@@#!$'
 
     elem = driver.find_element_by_xpath('//*[@id="id"]')
     elem.send_keys(login_ID)
@@ -66,74 +64,153 @@ try:
 except:
     pass
 
-# kill popup
+# kill popup1
 try:
     killPopUp('/html/frameset/frame','//*[@id="modalIframeId"]','//*[@id="noMoreToday"]')
 except:
     pass
 
-# click EAS2 classIN button in main frame
+# kill popup2
+try:
+    killPopUp('/html/frameset/frame','//*[@id="modalIframeId"]','//*[@id="noMoreToday"]')
+except:
+    pass
+
+
+# click Class '오픈소스소프트웨어실습'
 try:
     driver.switch_to.default_content()
-    # EAS2
+    # 오픈소스소프트웨어실습 
     # //*[@id="mCSB_1_container"]/ul/li[1]/a/span[2]/button 
     driver.switch_to.frame(driver.find_element_by_xpath('/html/frameset/frame'))
     eas2 = driver.find_element_by_xpath('//*[@id="mCSB_1_container"]/ul/li[2]/a/span[2]/button')
     eas2.click()
-    print("EAS2 -- 1")
+    print("오픈소스소프트웨어실습 -- 1")
 except:
     pass
 
-# click
+# click1
 try:
     driver.switch_to.default_content()
-    # EAS2
-    # //*[@id="mCSB_1_container"]/ul/li[1]/a/span[2]/button # EAS2-1
-    #//*[@id="listBox"]/table/tbody/tr[1]/td[1]/div[1]/ul[3]/li/a # EAS2-2
+    # 오픈소스소프트웨어실습 1번째 X Path
+    # //*[@id="listBox"]/table/tbody/tr[1]/td[2]/div[1]/ul[3]/li/a
     driver.switch_to.frame(driver.find_element_by_xpath('/html/frameset/frame'))
-    eas2_1 = driver.find_element_by_xpath('//*[@id="listBox"]/table/tbody/tr[1]/td[1]/div[1]/ul[3]/li/a')
-    eas2_1.click()
-    print("EAS2 -- 2")
+    osw = driver.find_element_by_xpath('//*[@id="listBox"]/table/tbody/tr[1]/td[1]/div[1]/ul[3]/li/a')
+    name = osw.get_attribute('text') # name이 '다시보기' 버튼인지 확인하기 위함.
+    print(name)
+    if name != "다시보기":
+        osw.click()
+    else:
+        # click2
+        try:
+            driver.switch_to.default_content()
+            # 오픈소스소프트웨어실습 2번째 X Path
+            # //*[@id="listBox"]/table/tbody/tr[1]/td[2]/div[2]/ul[3]/li/a  
+            driver.switch_to.frame(driver.find_element_by_xpath('/html/frameset/frame'))
+            osw = driver.find_element_by_xpath('//*[@id="listBox"]/table/tbody/tr[1]/td[2]/div[2]/ul[3]/li/a')
+            osw.click()
+            print("오픈소스소프트웨어실습 -- 2")
+        except:
+            pass
+    
 except:
     pass
 
+# 오픈소스소프트웨어실습 참여하기 버튼 누르기
 try:
     driver.switch_to.default_content()
-    # EAS2
-    # //*[@id="mCSB_1_container"]/ul/li[1]/a/span[2]/button # EAS2-1
-    #//*[@id="listBox"]/table/tbody/tr[1]/td[1]/div[1]/ul[3]/li/a # EAS2-2
     driver.switch_to.frame(driver.find_element_by_xpath('/html/frameset/frame'))
     driver.switch_to.frame(driver.find_element_by_xpath('//*[@id="reviewForm"]')) # iframe
     eas2_1 = driver.find_element_by_xpath('//*[@id="div_virtual"]/div/ul/li[1]/a')
     eas2_1.click()
-    print("EAS2 -- 3")
+    print("오픈소스소프트웨어실습 -- 3")
 except:
     pass
 time.sleep(5)
-# click meeting join
-try:
-    driver.switch_to.default_content()
-    # EAS2
-    eas2_1 = driver.find_element_by_xpath('//*[@id="smartJoinButton"]')
-    eas2_1.click()
-except:
-    pass
-
 
 # click meeting join
-try:
-    driver.switch_to.default_content()
-    driver.switch_to.frame(driver.find_element_by_xpath('//*[@id="pbui_iframe"]'))
+# try:
+#     # driver.get("https://dongguk.webex.com/webappng/sites/dongguk/meeting/info/39c7ae6dcd4eb9fa55538a4a3d837966")
+#     driver.switch_to.default_content()
+#     # EAS2 
+#     eas2_1 = driver.find_element_by_xpath('//*[@id="smartJoinButton"]/span')
+#     eas2_1.click()
+# except:
+#     pass
+
+def button_click(img_path):
+    ok = True
+    t = time.time()
+    while ok:
+        try:
+            btnLocation = pg.locateOnScreen(img_path)
+            point = pg.center(btnLocation) # 화면에서 이미지와 같은 것을 찾아서 중간 좌표값 반환
+            pg.click(point.x, point.y)
+            ok = False
+        except:
+            if time.time()-t > 6: 
+                ok = False
+            
+
+# # click meeting join-1
+# button5location = pg.locateOnScreen('/home/xi-jjun/ex_workplace/D-Coder/class_auto_login/Configuration/join_meeting.png')
+# point = pg.center(button5location) # 화면에서 이미지와 같은 것을 찾아서 중간 좌표값 반환
+# pg.click(point.x, point.y)
+
+# # camera, voice allow button click
+# button5location = pg.locateOnScreen('/home/xi-jjun/ex_workplace/D-Coder/class_auto_login/Configuration/allow.png')
+# point = pg.center(button5location) # 화면에서 이미지와 같은 것을 찾아서 중간 좌표값 반환
+# pg.click(point.x, point.y)
+
+button_click('/home/xi-jjun/ex_workplace/D-Coder/class_auto_login/Configuration/join_meeting.png') # click meeting join-1
+time.sleep(5)
+button_click('/home/xi-jjun/ex_workplace/D-Coder/class_auto_login/Configuration/allow.png') # camera, voice allow button click
+# ok = False
+# while ok:
+#     try:
+#         time.sleep(2)
+#         button_click('/home/xi-jjun/ex_workplace/D-Coder/class_auto_login/Configuration/video.png') # video off click
+#         ok = True
+#     finally:
+#         pass
+
+time.sleep(2)
+button_click('/home/xi-jjun/ex_workplace/D-Coder/class_auto_login/Configuration/video.png') # video off click
+
+time.sleep(1)
+# button_click('/home/xi-jjun/ex_workplace/D-Coder/class_auto_login/Configuration/voice2.png') # voice off click
+pg.moveTo(725,1047) # move to voice off
+pg.click()
+
+time.sleep(1)
+# button_click('/home/xi-jjun/ex_workplace/D-Coder/class_auto_login/Configuration/join-inner-meeting.png') # join meeting
+pg.moveTo(1047,1046) # move to join meeting
+pg.click()
+
+
+
+
+
+
+
+
+
+
+
+# # click meeting join
+# try:
+#     driver.switch_to.default_content()
+#     driver.switch_to.frame(driver.find_element_by_xpath('//*[@id="pbui_iframe"]'))
     
-    # EAS2
-    eas2_1 = driver.find_element_by_xpath('//*[@id="interstitial_join_btn"]')
-    eas2_1.click()
-except:
-    pass
+#     # EAS2
+#     eas2_1 = driver.find_element_by_xpath('//*[@id="interstitial_join_btn"]')
+#     eas2_1.click()
+# except:
+#     pass
 
-def frame_2_default():
-    pass
+# def frame_2_default():
+#     pass
 
-def frame_2_(frame_name):
-    pass
+# def frame_2_(frame_name):
+#     pass
 
--- dgu-food/README.md
+++ dgu-food/README.md
@@ -1,1 +1,7 @@
 # 동국대학교 학식 알려주는 텔레그램 챗 봇
+![img1](https://github.com/xi-jjun/myHobby/blob/main/img/img1.jpeg?raw=True){: width="55%"}
+![img2](https://github.com/xi-jjun/myHobby/blob/main/img/img2.jpeg?raw=True){: width="55%"}<br>
+특정 명령어를 입력하면 파이썬 코드로 이를 해석하고 해당 명령어에 대한 결과를 텔레그램 챗 봇에게 전달하여 사용자가 볼 수 있게 해줍니다.<br><br>
+개발 환경<br>
+OS : Ubuntu20.04<br>
+사용언어 : Python3.8
